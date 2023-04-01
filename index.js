35d693c84593926db1c0a699502f460d4ada6857
MW1R1 <nickmwirigi58@gmail.com>
2023-04-01 22:44:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
 }
+//function receive argument and return true
+//return false if not
+//declare function
+//callback for returning true or fals
+//testing with example
 
 /* 
   Add your pseudocode here
 */
+function isPalindrome(s) {
+  return s === s.split('').reverse().join('');
+}
 
 /*
   Add written explanation of your solution here
 */
-
+//create function
+//return true or false
+//test function
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  //testing with provided examples
+  console.log(isPalindrome("madam"));  // true
+console.log(isPalindrome("robot"));  // false
+console.log(isPalindrome("madam"));  // true
+console.log(isPalindrome("robot"));  // false
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
